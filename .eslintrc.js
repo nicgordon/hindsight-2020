914b3e8795cca8eaa242944e6844ba063ef4e4c8
Nic Gordon <nic.gordon@inlight.com.au>
2020-04-05 04:59:34 +1000

    Relax anchor is valid rule
    Necessary because of how next.js links expect anchors to be used

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended', 'prettier/react'],
   rules: {
+    'jsx-a11y/anchor-is-valid': [0],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-props-no-spreading': [1, { custom: 'ignore' }],
   },
