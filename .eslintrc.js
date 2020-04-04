5f9214af2231edfea4434e1af06046f4b161aabd
Nic Gordon <nic.gordon@inlight.com.au>
2020-04-05 04:18:43 +1000

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended', 'prettier/react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
