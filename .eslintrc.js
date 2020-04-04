dc8f912ca5384dc2ffcb58db889a0ca0308d05d5
Nic Gordon <nic.gordon@inlight.com.au>
2020-04-05 04:38:50 +1000

    Relax no spreading rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended', 'prettier/react'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': [1, { custom: 'ignore' }],
   },
 };
