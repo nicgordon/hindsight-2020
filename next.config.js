b52b58c9c66971c0f99cd7395e26d34c59baa46a
Nic Gordon <nic.gordon@inlight.com.au>
2020-04-09 15:13:29 +1000

    Allow for db connection to use heroku connection string

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   env: {
+    DATABASE_URL: process.env.DATABASE_URL,
     DB_HOST: process.env.DB_HOST,
     DB_PORT: process.env.DB_PORT,
     DB_USER: process.env.DB_USER,
