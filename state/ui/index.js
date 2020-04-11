e55d52974e4ff52efe76d11406d2d35d7c7208a3
Nic Gordon <nic.gordon@inlight.com.au>
2020-04-11 10:18:13 +1000

    Add edit mode state

--- state/ui/index.js
+++ state/ui/index.js
@@ -4,6 +4,7 @@ import get from 'lodash/get';
 import isUndefined from 'lodash/isUndefined';
 
 const initialState = {
+  isEditModeActive: false,
   isMenuOpen: false,
 };
 
@@ -11,8 +12,15 @@ const uiSlice = createSlice({
   name: 'todos',
   initialState,
   reducers: {
+    toggleEditMode(state, action) {
+      state.isEditModeActive = isUndefined(get(action, 'payload.isActive'))
+        ? !state.isEditModeActive
+        : action.payload.isActive;
+    },
     toggleMenu(state, action) {
-      state.isMenuOpen = isUndefined(get(action, 'payload.isOpen')) ? !state.isMenuOpen : action.payload.isOpen;
+      state.isMenuOpen = isUndefined(get(action, 'payload.isOpen'))
+        ? !state.isMenuOpen
+        : action.payload.isOpen;
     },
   },
 });
